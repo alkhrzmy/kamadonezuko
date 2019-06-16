bd5e414e443e28719cd086a7181789d32d53b9dc
rzmy <46349755+alkhrzmy@users.noreply.github.com>
2019-06-16 11:31:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ Sample bot t.me/kamadobot
 import telepot
 import time
 from telepot.loop import MessageLoop
+programStart = time.time()
 
 token = "844244943:AAGzMVzum7nTCrqLDr50Vccpu_ieco3RC30"
 bot = telepot.Bot(token)
@@ -16,7 +17,7 @@ my_id = '694351915'
 
 def handle(self):
         # print(self)
-        content_type, chat_type, chat_id = telepot.glance(self)
+        content_type, chat_type, chat_id, message_id = telepot.glance(self)
 
         print("_+ Message arrived: " + self['text'])
         command = self['text'].strip().lower()
@@ -25,7 +26,13 @@ def handle(self):
         elif command == '/speed':
           startTime = time.time()
           elapseTime = time.time() - startTime
-          bot.sendMessage(chat_id, 'Bot speed took %s second' % str(elapseTime))
+          bot.sendMessage(chat_id, 'Bot speed took %f second' % elapseTime)
+        elif command == '/check':
+                startTime = time.time()
+                bot.sendMessage(chat_id, 'Please wait...')
+                elapseTime = time.time() - startTime
+                timeRunning = time.time() - programStart
+                bot.sendMessage(chat_id, 'Bot already running %f\nBot speed is %f\nUsing: Heroku' % (timeRunning, elapseTime))
         else:
           bot.sendMessage(chat_id, "Hi! thanks for your message. Your message is " + self['text'])
 
